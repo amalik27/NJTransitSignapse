0135f99bd5efd637f9f5f0123d9c9fb78da0029b
Siddiq Mohammed <msm2029@gmail.com>
2023-06-07 13:21:54 -0400

    Added other fields with logic for adding and removing fields for stops

--- njt-signapse/src/components/form.js
+++ njt-signapse/src/components/form.js
@@ -7,6 +7,9 @@ function App() {
   const [destination, setDestination] = useState("");
   const [line, setLine] = useState("");
   const [tracks, setTracks] = useState("");
+  const [direction, setDirection] = useState("");
+  const [stops, setStops] = useState([{ id: "stop1", value: "" }]);
+  const [lastStop, setLastStop] = useState("");
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -33,6 +36,31 @@ function App() {
     console.log("Destination:", destination);
     console.log("Line:", line);
     console.log("Tracks:", tracks);
+    console.log("Direction:", direction);
+    stops.forEach((stop) => {
+      console.log(
+        `${stop.id.charAt(0).toUpperCase() + stop.id.slice(1)}:`,
+        stop.value
+      );
+    });
+    console.log("Last Stop:", lastStop);
+  };
+
+  const handleAddStop = () => {
+    const newStopId = `stop${stops.length + 1}`;
+    setStops([...stops, { id: newStopId, value: "" }]);
+  };
+
+  const handleRemoveStop = (stopId) => {
+    const updatedStops = stops.filter((stop) => stop.id !== stopId);
+    setStops(updatedStops);
+  };
+
+  const handleStopChange = (e, stopId) => {
+    const updatedStops = stops.map((stop) =>
+      stop.id === stopId ? { ...stop, value: e.target.value } : stop
+    );
+    setStops(updatedStops);
   };
 
   return (
@@ -46,6 +74,7 @@ function App() {
               value={agency}
               onChange={(e) => setAgency(e.target.value)}
               name="agency"
+              required
             >
               <option value="">Select an option</option>
               {data.length > 0 &&
@@ -66,6 +95,7 @@ function App() {
               value={destination}
               onChange={(e) => setDestination(e.target.value)}
               name="destination"
+              required
             >
               <option value="">Select an option</option>
               {data.length > 0 &&
@@ -86,6 +116,7 @@ function App() {
               value={line}
               onChange={(e) => setLine(e.target.value)}
               name="line"
+              required
             >
               <option value="">Select an option</option>
               {data.length > 0 &&
@@ -106,6 +137,7 @@ function App() {
               value={tracks}
               onChange={(e) => setTracks(e.target.value)}
               name="tracks"
+              required
             >
               <option value="">Select an option</option>
               {data.length > 0 &&
@@ -118,6 +150,82 @@ function App() {
                 )}
             </select>
           </div>
+
+          <div className="select-container">
+            <label htmlFor="direction">Direction:</label>
+            <select
+              id="direction"
+              value={direction}
+              onChange={(e) => setDirection(e.target.value)}
+              name="direction"
+              required
+            >
+              <option value="">Select an option</option>
+              {data.length > 0 &&
+                data.map((item) =>
+                  item["Direction"].map((option, index) => (
+                    <option key={`direction_${index}`} value={option}>
+                      {option}
+                    </option>
+                  ))
+                )}
+            </select>
+          </div>
+
+          {stops.map((stop) => (
+            <div className="select-container" key={stop.id}>
+              <label htmlFor={stop.id}>{`${
+                stop.id === "stop1" ? "First" : "Stop " + stop.id.slice(4)
+              }`}</label>
+              <select
+                id={stop.id}
+                value={stop.value}
+                onChange={(e) => handleStopChange(e, stop.id)}
+                name={stop.id}
+                required
+              >
+                <option value="">Select an option</option>
+                {data.length > 0 &&
+                  data.map((item) =>
+                    item["Stop"].map((option, index) => (
+                      <option key={`stop_${index}`} value={option}>
+                        {option}
+                      </option>
+                    ))
+                  )}
+              </select>
+              {stop.id !== "stop1" && (
+                <button type="button" onClick={() => handleRemoveStop(stop.id)}>
+                  Remove
+                </button>
+              )}
+            </div>
+          ))}
+
+          <div className="select-container">
+            <label htmlFor="lastStop">Last Stop:</label>
+            <select
+              id="lastStop"
+              value={lastStop}
+              onChange={(e) => setLastStop(e.target.value)}
+              name="lastStop"
+              required
+            >
+              <option value="">Select an option</option>
+              {data.length > 0 &&
+                data.map((item) =>
+                  item["Stop"].map((option, index) => (
+                    <option key={`stop_${index}`} value={option}>
+                      {option}
+                    </option>
+                  ))
+                )}
+            </select>
+          </div>
+
+          <button type="button" onClick={handleAddStop}>
+            Add Stop
+          </button>
 
           <button type="submit">Submit</button>
         </form>
